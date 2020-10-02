b5bd9c3165869d72dbae75199e0a36e711e395c3
Yugandhar Shashikant Salvi <60753349+Yugandhar-S@users.noreply.github.com>
2020-10-02 13:25:59 +0530

    Create Register.java

---
+++ Hibernate/Register via.Hibernate/Register.java
@@ -0,0 +1,52 @@
+package mypack;
+
+import javax.persistence.*;
+
+@Entity
+public class Register {
+	private int id;
+	private String name;
+	private String address;
+	private String email;
+	private String password;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Register [id=" + id + ", name=" + name + ", address=" + address + ", email=" + email + ", password="
+				+ password + "]";
+	}
+	
+	
+}
