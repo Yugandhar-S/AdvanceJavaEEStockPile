0e216412b9ad1da058f8aaefd14fbaa90d45d47b
Yugandhar Shashikant Salvi <60753349+Yugandhar-S@users.noreply.github.com>
2020-10-05 12:54:24 +0530

    Entity Class

---
+++ Hibernate/ProductReverse/Department.java
@@ -0,0 +1,46 @@
+package mypack;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dept")
+public class Department {
+
+	private int deptno;
+	private String dname;
+	private String loc;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "deptno")
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	@Override
+	public String toString() {
+		return "Departmanent number=" + deptno + ", department name=" + dname + " and location=" + loc;
+	}
+	
+	
+	
+}
