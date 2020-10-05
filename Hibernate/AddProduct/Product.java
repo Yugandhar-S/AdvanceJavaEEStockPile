50ec58cbbce49693db67b2354507234c83398b70
Yugandhar Shashikant Salvi <60753349+Yugandhar-S@users.noreply.github.com>
2020-10-05 10:42:18 +0530

    Product Entity

---
+++ Hibernate/AddProduct/Product.java
@@ -0,0 +1,52 @@
+package mypack;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	private int pid;
+	private String pname;
+	private int quantity;
+	private int price;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product pid=" + pid + ", pname=" + pname + ", quantity=" + quantity + ", price=" + price;
+	}
+	
+	
+	
+	
+}
